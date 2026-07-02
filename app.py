8cb0de79b6f0db95afdb3f47e280a6ee206b534b
Haiduc Georgiana <georgianahaiduc73@gmail.com>
2026-07-02 17:22:16 +0300

    .

--- app.py
+++ app.py
@@ -1,28 +1,25 @@
 import streamlit as st
-
 from ui_components import afiseaza_interfata
-
 # from ai_integration import optimizeaza_text_experienta
-#
 # from ats_scanner import calculeaza_scor_ats
-
 from pdf_generator import genereaza_pdf_cv
 
-
 date = afiseaza_interfata()
-pdf = genereaza_pdf_cv(
-    {
-        "nume": date["nume"],
-        "email": date["email"],
-        "status": date["status"],
-        "educatie": date["educatie"],
-        "tehnologii": date["tehnologii"]
-    },
-    date["experienta_bruta"] # trebuie schimbat cu textul optimizat de AI
-)
-st.download_button(
-    "Descarcă CV",
-    pdf,
-    "CV.pdf",
-    mime="application/pdf"
-)
+if date and "nume" in date and date["nume"]:
+    pdf = genereaza_pdf_cv(
+        {
+            "nume": date["nume"],
+            "email": date["email"],
+            "status": date["status"],
+            "educatie": date["educatie"],
+            "tehnologii": date["tehnologii"]
+        },
+        date["experienta_bruta"] # trebuie schimbat cu textul optimizat de AI
+    )
+    
+    st.download_button(
+        "Descarcă CV",
+        pdf,
+        "CV.pdf",
+        mime="application/pdf"
+    )
